75b02584d3228caece1b6873027a293bc77188b4
quinho <quinho@quinho-notebook>
2013-05-03 15:39:39 -0300

    Primeiro commit

--- app/Model/Estado.php
+++ app/Model/Estado.php
@@ -1,4 +1,25 @@
 <?php
 
-class Estado extends AppModel {}
+class Estado extends AppModel {
+    
+    public $validate = array(
+        'nome' => array(
+            'rule' => 'notEmpty',
+            'message' => 'Este campo não pode ser vazio.'
+        ),
+        'sigla' => array(
+            'notEmpty' => array(
+                'rule'     => 'notEmpty',
+                'required' => true,
+                'message'  => 'Este campo não pode ser vazio.'
+            ),
+            'tamanho' => array(
+                'rule'    => array('minLength', '2'),
+                'message' => 'Este campo deve possuir 2 caracteres.'
+            )
+        ),
+    );
+    
+}
+
 ?>
